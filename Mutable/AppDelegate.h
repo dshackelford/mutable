03ceb903107332c86f1c6640bff1c185dc97a6ed
Dylan Shackelford <Dshack@Dylans-MacBook-Pro.local>
2015-03-18 15:09:29 -0700

    Initial Commit

---
+++ Mutable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mutable
+//
+//  Created by Dylan Shackelford on 3/18/15.
+//  Copyright (c) 2015 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mutable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mutable
+//
+//  Created by Dylan Shackelford on 3/18/15.
+//  Copyright (c) 2015 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
